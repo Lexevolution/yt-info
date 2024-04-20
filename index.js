f9e56b1055b2f484e88d44a2656145898c610ce5
Lexevolution <31176843+Lexevolution@users.noreply.github.com>
2024-04-21 06:05:35 +1000

    Add project files

---
+++ index.js
@@ -0,0 +1,25 @@
+import { Innertube, UniversalCache } from 'youtubei.js';
+import Express from 'express';
+const app = Express();
+const port = 33000;
+const yt = await Innertube.create({
+    cache: new UniversalCache(false)
+});
+app.get('/endcards', async (req, res) => {
+    if (typeof req.query.id === "string") {
+        try {
+            var vid = await yt.getInfo(req.query.id);
+            var endcards = vid.endscreen?.elements;
+            res.json(endcards);
+        }
+        catch {
+            res.status(404).send("Video not found");
+        }
+    }
+    else {
+        res.status(404).send("Video not found");
+    }
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
